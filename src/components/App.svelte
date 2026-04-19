66839cf449974447fdae7b45ad3420a590342322
phietttt <tranphiet4349@gmail.com>
2026-04-19 12:07:27 -0700

    initial proj

---
+++ src/components/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { bruinBucks } from "../game/state";
+</script>
+
+<main>
+  <p>{$bruinBucks} Bruin Bucks</p>
+  <button onclick={() => bruinBucks.update((n) => n + 1)}>
+    <img src="/ucla-logo.png" alt="Bruin" width="150" />
+  </button>
+</main>
+
+<style>
+  button {
+    background: none;
+    border: none;
+    padding: 0;
+    cursor: pointer;
+  }
+</style>
